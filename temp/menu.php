da724449b085367b9d65681b6ab1a457a85e92af
cesarmoralesg <44750318+cesarmoralesg@users.noreply.github.com>
2023-04-17 12:47:45 -0600

    Update menu.php

--- temp/menu.php
+++ temp/menu.php
@@ -58,7 +58,7 @@
 				</a>
 				<div class="collapse" id="collapseListas" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
 					<nav class="sb-sidenav-menu-nested nav">
-					    <a class="nav-link" href="<?php echo $server_name; ?>index"><i class="bi bi-file-earmark-pdf"></i>&nbsp;&nbsp;Lista</a>
+					    <a class="nav-link" href="<?php echo $server_name; ?>index.php"><i class="bi bi-file-earmark-pdf"></i>&nbsp;&nbsp;Lista</a>
 					</nav>
 				</div>
 				<!-- Fin Lista de Alumnos -->
