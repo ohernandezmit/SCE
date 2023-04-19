fbfc012dd9ef071e66da2986b3e3cfd1bca51a2e
CesarAMG <ingcesar.moralesg@gmail.com>
2023-04-19 10:18:04 -0600

    Update excel.php

--- students_list/excel.php
+++ students_list/excel.php
@@ -1,4 +1,54 @@
-<?php 
+<?php ini_set("display_errors","1");
+
+/** Error reporting */
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
+date_default_timezone_set('America/Mexico_City');
+if (PHP_SAPI == 'cli')
+	die('This example should only be run from a Web Browser');
+include "../db/var.php";
+include "../db/conect.php";
+require 'https://srv174.hstgr.io:7443/9d74db396c492fa7/files/public_html/sys/vendor/autoload.php';
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\Worksheet\Drawing;
+use PhpOffice\PhpSpreadsheet\Cell\DataType;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
+use PhpOffice\PhpSpreadsheet\Style\Color;
+use PhpOffice\PhpSpreadsheet\Style\Border;
+
 $fecha = date('d/m/Y');
-echo $grado = $_POST['grado'];
+$grado = $_POST['grado'];
+
+$mysqli->set_charset("utf8");
+
+$spreadsheet = new Spreadsheet();
+$spreadsheet->getProperties()->setCreator("Technology MIT")
+	->setLastModifiedBy("echnology MIT")
+	->setTitle("")
+	->setSubject("PHPExcel Test Document")
+	->setDescription("Este Reporte fue generado por el Sistema de Control Escolar")
+	->setKeywords("office PHPExcel php")
+	->setCategory("Report file");
+
+function cellColor($cells, $color)
+{
+	global $spreadsheet;
+	$spreadsheet->getActiveSheet()->getStyle($cells)->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setARGB($color);
+}
+$cols=array('','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+'AA','AB','AC','AD','AE','AF','AG','AH','AI','AJ','AK','AL','AM','AN','AO','AP','AQ','AR','AS','AT','AU','AV','AW','AX','AY','AZ',
+'BA','BB','BC','BD','BE','BF','BG','BH','BI','BJ','BK','BL','BM','BN','BO','BP','BQ','BR','BS','BT','BU','BV','BW','BX','BY','BZ',
+'CA','CB','CC','CD','CE','CF','CG','CH','CI','CJ','CK','CL','CM','CN','CO','CP','CQ','CR','CS','CT','CU','CV','CW','CX','CY','CZ',
+'DA','DB','DC','DD','DE','DF','DG','DH','DI','DJ','DK','DL','DM','DN','DO','DP','DQ','DR','DS','DT','DU','DV','DW','DX','DY','DZ',
+'EA','EB','EC','ED','EE','EF','EG','EH','EI','EJ','EK','EL','EM','EN','EO','EP','EQ','ER','ES','ET','EU','EV','EW','EX','EY','EZ');
+
+//include "consulta1xx.php";
+
+
 ?>
