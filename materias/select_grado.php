f7fd5208e6cc403af7ac5f61d9778260a76d1fa7
CesarAMG <ingcesar.moralesg@gmail.com>
2023-04-21 11:26:20 -0600

    Update select_grado.php

--- materias/select_grado.php
+++ materias/select_grado.php
@@ -4,7 +4,7 @@ include "../db/conect.php";
 
 $nivel = $_POST['nivel'];
 
-	$queryG = "SELECT DISTINCT Id, grado FROM grados_grupos WHERE Id_nivel = '$nivel'";
+	$queryG = "SELECT Id, grado, grupos FROM grados_grupos WHERE Id_nivel = '$nivel'";
 	$rstG = $mysqli->query($queryG);
 	
 	$cadena = "<select id='grado' name='grado' class='form-control'>";
@@ -13,7 +13,7 @@ $nivel = $_POST['nivel'];
 		echo $cadena = $cadena."<option selected disabled value=''>Sin registros</option>";
 	}else{
 		while($rowG = $rstG->fetch_array(MYSQLI_ASSOC)) {
-			$cadena = $cadena.'<option value='.$rowG['Id'].'>'.$rowG['grado'].'</option>';
+			$cadena = $cadena.'<option value='.$rowG['Id'].'>'.$rowG['grado'].'° '.$rowG['grupos'].'</option>';
 		}
 		echo $cadena."</select>";
 	}
