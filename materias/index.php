7f81f4b7a4bd99afa0508badd290f17729f634e7
OmarHdzB <128820243+OmarHdzB@users.noreply.github.com>
2023-04-27 12:18:01 -0600

    Update index.php

--- materias/index.php
+++ materias/index.php
@@ -43,18 +43,20 @@ $fecha = date('Y/m/d');
 									<th class="text-center">Nivel</th>
 									<th class="text-center">Grado</th>
 									<th class="text-center">Materia</th>
+									<th class="text-center">Docente</th>
 								</tr>
 							</thead>
 							<tbody>
 								<?php
-								$sq="SELECT a.*, b.nombre, g.grado, g.grupos 
+								$sq="SELECT a.*, b.nombre, g.grado, g.grupos, u.Id as Id_maestro, u.nombre as nombre_maestro, u.apellidos 
 									 FROM materia as a
 									 INNER JOIN niveles as b on b.Id = a.Id_nivel
 									 INNER JOIN grados_grupos as g on g.Id = a.Id_grado
+									 INNER JOIN usuarios as u on u.Id = a.id_maestro
 									 WHERE a.activo='0'";
 								$rs=$mysqli->query($sq);
 								while ($row=$rs->fetch_array(MYSQLI_ASSOC)) {
-									//if ($row['estatus']==1) { $activo = "Activo"; } else { $activo = "Inactivo"; }
+									if ($row['Id_maestro']==0) { $nombre_maestro = "Sin docente asignado"; } else { $nombre_maestro = $row['nombre_maestro'].' '.$row['apellidos']; }
 									
 									$edit = '<div class="btn-group">
 		                                <button type="button" class="btn btn-outline-success btn2" data-bs-toggle="dropdown" aria-expanded="false" >
@@ -72,6 +74,7 @@ $fecha = date('Y/m/d');
 									<th><?php echo $row['nombre']; ?></th>
 									<td><?php echo $row['grado'].'° '.$row['grupos']; ?></td>
 									<td><?php echo $row['materia']; ?></td>
+									<td><?php echo $nombre_maestro; ?></td>
 									<td class="text-center"><?php echo $edit.'&nbsp;&nbsp;'.$delete; ?></td>
 									 <?php include "EditarMateria.php"; include "DeleteModal.php";  ?>
 								</tr>
